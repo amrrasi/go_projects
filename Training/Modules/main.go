9967684df9818e87c514888eb0907f81cca62321
amrrasi <amirrezaaskari2003@gmail.com>
2026-07-29 10:26:47 +0330

    finish working modules

--- Training/Modules/main.go
+++ Training/Modules/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/amrrasi/go_projects/services"
 	jalali "github.com/jalaali/go-jalaali"
 )
 
@@ -10,9 +11,12 @@ func main() {
 	fmt.Printf("Hello World! \n")
 	year, month, day, error := jalali.ToGregorian(1404, 06, 05)
 	if error == nil {
-		fmt.Printf("Shamsi Year is %d/%d/%d", year, month, day)
+		fmt.Printf("Shamsi Year is %d/%d/%d\n", year, month, day)
 	} else {
-		fmt.Printf("Shamsi Year ba moshkel movajeh shode")
+		fmt.Printf("Shamsi Year ba moshkel movajeh shode\n")
 	}
+
+	var services services.TestService = services.TestService{}
+	fmt.Printf("%v", services)
 
 }
