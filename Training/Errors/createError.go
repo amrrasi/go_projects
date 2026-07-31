cd8fd343853605e8b82d84f46d06650dddce506b
amrrasi <amirrezaaskari2003@gmail.com>
2026-07-31 21:05:32 +0330

    error creating

--- Training/Errors/createError.go
+++ Training/Errors/createError.go
@@ -1,7 +1,37 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"io"
-	"net/http"
+	//"io"
+	//"net/http"
 )
+
+func main() {
+
+	output, err := DisplayErrors(5)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(output)
+
+}
+
+func DisplayErrors(Number int) (int, error) {
+
+	if Number == 0 {
+		return 0, errors.New("number is not valid it is 0")
+	}
+	return Number * 2, nil
+
+}
+
+func DisplayErrors2(Number int) (int, error) {
+
+	if Number == 0 {
+		return 0, fmt.Errorf("number is not valid it is 0 %d", Number)
+	}
+	return Number * 2, nil
+
+}
