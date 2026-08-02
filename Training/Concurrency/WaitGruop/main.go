97de8587567762adff85cfb615f13063a6b82565
amrrasi <amirrezaaskari2003@gmail.com>
2026-08-02 12:03:05 +0330

    waitgroup fertisch

--- Training/Concurrency/WaitGruop/main.go
+++ Training/Concurrency/WaitGruop/main.go
@@ -1,28 +1,46 @@
 package main
 
 import (
-	"github.com/go-test/deep"
+	//"github.com/go-test/deep"
+	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+	"sync"
 )
 
 var Todolist = []string{}
 
 func main() {
 
+	wg := sync.WaitGroup{}
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		GetTodo(i, &wg)
+	}
+	fmt.Printf("%v", Todolist)
+
+	wg.Wait()
 }
 
-func getUrl() {
-	response, err := http.Get("https://fanapit.com")
+func GetTodo(id int, wg *sync.WaitGroup) {
+	GetUrl("https://jsonplaceholder.typicode.com/todos/"+strconv.Itoa(id), wg)
+}
+
+func GetUrl(url string, wg *sync.WaitGroup) {
+
+	defer wg.Done()
+
+	response, err := http.Get(url)
 	if err != nil {
-		panic(error)
+		panic(err)
 	}
 
 	responseBody, err := io.ReadAll(response.Body)
 	defer response.Body.Close()
 
 	if err != nil {
-		panic(error)
+		panic(err)
 	}
 
 	Todolist = append(Todolist, string(responseBody))
