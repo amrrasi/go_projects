1b4b59750ce661267ba6efe14471fa671795457c
amrrasi <amirrezaaskari2003@gmail.com>
2026-08-11 10:24:52 +0330

    finish marshal1

--- Training/Json/Marshall/main.go
+++ Training/Json/Marshall/main.go
@@ -3,22 +3,25 @@ package main
 import "encoding/json"
 
 type Person struct {
-	Name   string `json:"name"`
-	Family string `json:"family"`
-	Age    int    `json:"age,omitempty"`
+	Name         string `json:"name"`
+	Family       string `json:"family"`
+	Age          int    `json:"age,omitempty"`
+	NationalCode string `json:"nationalCode"`
 }
 
 func main() {
 
-	Person1 := Person{Name: "Amir", Family: "Askari", Age: 22}
-	Person2 := Person{Name: "Ali", Family: "izi", Age: 0}
+	Person1 := Person{Name: "Amir", Family: "Askari", Age: 22, NationalCode: "0250248786"}
+	Person2 := Person{Name: "Ali", Family: "izi", NationalCode: "1111111111"}
+	Person3 := Person{Name: "Bardia", Family: "Biggy", Age: 10, NationalCode: "2222222222"}
+	Person4 := Person{Name: "Mamad", Family: "Seifi", Age: 66, NationalCode: "3333333333"}
 
-	persons := []Person{Person1, Person2}
-	PersonJson, err := json.Marshal(persons)
+	Persons := []Person{Person1, Person2, Person3, Person4}
+	personJson, err := json.Marshal(Persons)
 	if err != nil {
 		panic(err)
 	}
 
-	println(string(PersonJson))
+	println(string(personJson))
 
 }
