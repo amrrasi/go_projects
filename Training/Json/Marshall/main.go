394e42854fdebf6b0cb453841aa30e14b6a39c57
amrrasi <amirrezaaskari2003@gmail.com>
2026-08-11 10:10:20 +0330

    json marshal

---
+++ Training/Json/Marshall/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "encoding/json"
+
+type Person struct {
+	Name   string `json:"name"`
+	Family string `json:"family"`
+	Age    int    `json:"age,omitempty"`
+}
+
+func main() {
+
+	Person1 := Person{Name: "Amir", Family: "Askari", Age: 22}
+	Person2 := Person{Name: "Ali", Family: "izi", Age: 0}cd 
+
+	Person1Json, err := json.Marshal(Person1)
+	if err != nil {
+		panic(err)
+	}
+
+	Person2Json, err := json.Marshal(Person2)
+	if err != nil {
+		panic(err)
+	}
+
+	println(string(Person1Json))
+	println(string(Person2Json))
+
+}
