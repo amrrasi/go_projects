4109f4b02d24f59a4ae7268d733362a1a41c3062
amrrasi <amirrezaaskari2003@gmail.com>
2026-08-11 10:14:21 +0330

    finish marshal

--- Training/Json/Marshall/main.go
+++ Training/Json/Marshall/main.go
@@ -13,17 +13,12 @@ func main() {
 	Person1 := Person{Name: "Amir", Family: "Askari", Age: 22}
 	Person2 := Person{Name: "Ali", Family: "izi", Age: 0}
 
-	Person1Json, err := json.Marshal(Person1)
+	persons := []Person{Person1, Person2}
+	PersonJson, err := json.Marshal(persons)
 	if err != nil {
 		panic(err)
 	}
 
-	Person2Json, err := json.Marshal(Person2)
-	if err != nil {
-		panic(err)
-	}
-
-	println(string(Person1Json))
-	println(string(Person2Json))
+	println(string(PersonJson))
 
 }
