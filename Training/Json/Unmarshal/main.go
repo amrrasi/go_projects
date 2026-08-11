2f433ecf534bb1371b4a543096740b0b9dda1a9e
amrrasi <amirrezaaskari2003@gmail.com>
2026-08-11 14:18:57 +0330

    Unmarshall

--- Training/Json/Unmarshal/main.go
+++ Training/Json/Unmarshal/main.go
@@ -1,1 +1,27 @@
 package main
+
+import "encoding/json"
+
+type person struct {
+	Name   string
+	Family string
+	Age    int
+}
+
+func main() {
+	UnmarshalExample()
+}
+
+func UnmarshalExample() {
+	person1Json := []byte(`{"name":"Ali","Family":"izi","Age":55}`)
+	var person1 = person{}
+	err := json.Unmarshal(person1Json, &person1)
+
+	if err != nil {
+		panic(err)
+	}
+
+	println(person1.Name)
+	println(person1.Family)
+	println(person1.Age)
+}
