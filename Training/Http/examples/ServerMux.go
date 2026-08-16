8ab3e36e042e8878bc9bdec6f0478083b7f62d21
amrrasi <amirrezaaskari2003@gmail.com>
2026-08-16 15:44:19 +0330

    add mux http

--- Training/Http/examples/ServerMux.go
+++ Training/Http/examples/ServerMux.go
@@ -1,25 +1,29 @@
 package examples
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
+type Testhandler struct {
+}
+
 func CreateServerWithMux() {
 
-	mux := http.NewServeMux()
-
-	mux.Handle("/google", http.RedirectHandler("https://www.google.com", 307))
-	mux.Handle("/yahoo", http.RedirectHandler("https://www.yahoo.com", 307))
 	server1 := http.Server{
 		Addr:         ":8080",
 		ReadTimeout:  time.Second * 10,
 		WriteTimeout: time.Second * 10,
-		Handler:      mux,
+		Handler:      Testhandler{},
 	}
 
 	err1 := server1.ListenAndServe()
 	if err1 != nil {
 		panic(err1)
 	}
+}
+
+func (h Testhandler) ServerHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
 }
