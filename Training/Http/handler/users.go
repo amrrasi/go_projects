a2859090a227892265e46d25efb98e6bab97bd2b
amrrasi <amirrezaaskari2003@gmail.com>
2026-08-17 09:58:23 +0330

    add all

--- Training/Http/handler/users.go
+++ Training/Http/handler/users.go
@@ -1,10 +1,40 @@
 package handler
 
-import "net/http"
+import (
+	"fmt"
+	"http-examples/model"
+	"net/http"
+)
 
 type UserHandler struct {
 }
 
 func (h UserHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
+	switch {
+
+	case r.Method == "GET" && len(r.URL.Query().Get("id")) > 0:
+		GetUser(w, r)
+		return
+	case r.Method == "Get" && len(r.URL.Query().Get("id")) == 0:
+		GetUserList(w, r)
+		return
+	case r.Method == "POST":
+		CreateUser(w, r)
+		return
+	}
+
+}
+
+func CreateUser(u model.User, w http.ResponseWriter, r *http.Request) {
+	authorizationKey := r.Header.Get("x-Authorization")
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	fmt.Println(w, "Get user with id:", id)
+}
+
+func GetUserList(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Get userList")
 }
